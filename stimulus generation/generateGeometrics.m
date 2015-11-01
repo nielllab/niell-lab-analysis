b0c88729fe13a5a0b03a5d1fe9c6917921c8e41b
SENDAK <niell.lab.orders@gmail.com>
2015-11-01 15:33:27 -0800

    modified stimulus generation
    geometric stim generation complete, Phil

--- stimulus generation/generateGeometrics.m
+++ stimulus generation/generateGeometrics.m
@@ -1,3 +1,17 @@
+%PRLP 10/29/2015 Niell Lab
+%This code is used to generate simple unidirectional stimuli based on
+%images made in Adobe Illustrator
+
+nstim = 5; %choose number of repeats per stimulus
+MovieMag=3;                 %% magnification that movie will be played at
+screenWidthPix = 1280;        %% Screen width in Pixels
+screenWidthCm = 50;         %% Width in cm
+screenDistanceCm = 25;      %% Distance in cm
+
+screenWidthDeg = 2*atan(0.5*screenWidthCm/screenDistanceCm)*180/pi;
+degperpix = (screenWidthDeg/screenWidthPix)*MovieMag;
+
+%load stimuli - 4 shapes, 2 sizes each, centered
 triBig = imread('geometric shapes-05.jpg');
 triSml = imread('geometric shapes-40.jpg');
 sqrBig = imread('geometric shapes-15.jpg');
@@ -7,7 +21,8 @@ crcSml = imread('geometric shapes-35.jpg');
 starBig = imread('geometric shapes-20.jpg');
 starSml = imread('geometric shapes-25.jpg');
 
-shapes = zeros(256,256,8); %8 basic starting shapes, 4 of 2 sizes each
+%load basic stimuli into one array
+shapes = zeros(256,256,8);
 shapes(:,:,1) = triBig;
 shapes(:,:,2) = triSml;
 shapes(:,:,3) = sqrBig;
@@ -17,20 +32,26 @@ shapes(:,:,6) = crcSml;
 shapes(:,:,7) = starBig;
 shapes(:,:,8) = starSml;
 
-stimuli = 127*ones(256,256,128,144);
-asp = size(stimuli,3);
-leg = zeros(144,3);
+scaleDownBy = 4;
+shapes = shapes(1:scaleDownBy:256,1:scaleDownBy:256,:);
+stimframes = 30;
+
+%create array to hold all generated stimuli
+stimlib = 127*ones(size(shapes,1),size(shapes,2),stimframes,144);
+asp = stimframes/2; %used to control amount of motion/duration of frame
+ISI = size(stimlib,3);
+leg = zeros(144,3); %legend containing info about each stimulus for later analysis
 
 cnt = 1;
-for a = 1:size(shapes,3)
+for a = 1:size(shapes,3) %for all starting stimuli
     for color = 1:2
-        if color==1
+        if color==1 %one black and one white, both on grey
             stim = shapes(:,:,a);
             stim(stim<=127) = 127; stim(stim>127) = 0;
-            for direc = 1:9
+            for direc = 1:9 % 9 directions (5 stationary, 4 moving out from center)
                 if direc==1
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = stim;
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = stim;
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -38,8 +59,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==2;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,-asp/2,1);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,-asp,1);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -47,8 +68,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==3;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,asp/2,2);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,asp,2);
                     end        
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -56,8 +77,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==4;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,asp/2,1);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,asp,1);
                     end 
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -65,8 +86,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==5;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,-asp/2,2);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,-asp,2);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -74,8 +95,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==6;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,asp/2-n,1);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,asp-n,1);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -83,8 +104,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==7;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,-asp/2+n,2);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,-asp+n,2);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -92,8 +113,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==8;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,-asp/2+n,1);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,-asp+n,1);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -101,8 +122,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==9;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,asp/2-n,2);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,asp-n,2);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -116,8 +137,8 @@ for a = 1:size(shapes,3)
             stim(stim<=127) = 127; stim(stim>127) = 255;
             for direc = 1:9
                 if direc==1
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = stim;
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = stim;
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -125,8 +146,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==2;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,-asp/2,1);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,-asp,1);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -134,8 +155,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==3;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,asp/2,2);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,asp,2);
                     end        
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -143,8 +164,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==4;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,asp/2,1);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,asp,1);
                     end 
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -152,8 +173,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==5;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,-asp/2,2);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,-asp,2);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -161,8 +182,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==6;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,asp/2-n,1);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,asp-n,1);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -170,8 +191,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==7;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,-asp/2+n,2);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,-asp+n,2);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -179,8 +200,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                  elseif direc==8;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,-asp/2+n,1);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,-asp+n,1);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -188,8 +209,8 @@ for a = 1:size(shapes,3)
                     leg(cnt,4) = direc;
                     cnt = cnt+1;
                 elseif direc==9;
-                    for n = 1:asp
-                        stimuli(:,:,n,cnt) = circshift(stim,asp/2-n,2);
+                    for n = 1:stimframes
+                        stimlib(:,:,n,cnt) = circshift(stim,asp-n,2);
                     end
                     leg(cnt,1) = cnt;
                     leg(cnt,2) = a;
@@ -202,8 +223,15 @@ for a = 1:size(shapes,3)
     end
 end
 
-                    
-% figure
-% for j = 1:128
-%     imshow(stimuli(:,:,j,6));
-% end
+leg = repmat(leg,nstim,1); %copy * nstim
+stimlib = repmat(stimlib,1,1,1,nstim); %copy * nstim
+
+%generate stimulus presentation order
+order = randperm(size(leg,1));
+ordleg = leg(order,:);
+ordstimlib = stimlib(:,:,:,order);
+
+% dir = 'C:\Users\nlab\Desktop\Stimuli';
+% nam = 'GeomStim';
+% save(fullfile(dir,nam),'ordstimlib','ordleg','nstim','shapes','degperpix','-v7.3');
+ 
