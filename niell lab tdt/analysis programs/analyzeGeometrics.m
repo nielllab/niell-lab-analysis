58ab26e39f743e811c6845c02039bba9d0ca2e07
SENDAK <niell.lab.orders@gmail.com>
2015-11-12 17:58:58 -0800

    created file to analyze geometric stimuli
    phil

---
+++ niell lab tdt/analysis programs/analyzeGeometrics.m
@@ -0,0 +1,7 @@
+%%%analyzeGeometrics
+%%%use to get spiking relative to geometric stimulus movie
+%%% P.R.L. Parker, Cris Niell Lab, 11/12/2015
+
+%use getMovieData to pull spike times, mouse velocity/time, frame info
+[spikes mouseT mouseV framenums frametimes] = getMovieData;
+
