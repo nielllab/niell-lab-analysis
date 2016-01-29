605cb44a3d6006e3270d4f40788da3a9bd705eab
Cris <crisniell@gmail.com>
2016-01-28 16:26:08 -0800

    more tiny

--- niell lab tdt/analysis programs/compile data/compileDOIdarkness.m
+++ niell lab tdt/analysis programs/compile data/compileDOIdarkness.m
@@ -11,6 +11,9 @@ use =  find( strcmp({files.treatment},'DOI') & strcmp({files.notes},'good data')
 
 sprintf('%d selected sessions',length(use))
 
+prespikes = []; %%% initialize data to be saved
+postspikes = [];
+
 for i = 1:length(use)
     afile = files(use(i)).analysisfile;
     clustfile = files(use(i)).clusterfile;
@@ -18,8 +21,11 @@ for i = 1:length(use)
     block = files(use(i)).prewn;   %%% run for pre
     darknessAnalysis
     %%% save out any important data
+    prespikes{end+1:end+length(blockSpike)} = blockSpike;
+    
     
     block = files(use(i)).postwn;  %%% run for post
     darknessAnalysis
     %%% save out any important data
+    postspikes{end+1:end+length(blockSpike)} = blockSpike;
 end
