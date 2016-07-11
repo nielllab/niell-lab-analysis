7496559c0d7052475c1882b81de529f12c99c0f5
SENDAK <niell.lab.orders@gmail.com>
2016-07-11 15:27:54 -0700

    removed hard frame coding for movies in psychstim
    phil

--- PsychStimController/PsychStimController.m
+++ PsychStimController/PsychStimController.m
@@ -582,9 +582,9 @@ elseif sync==kWidefield
     % p.n = round(.75*numSecs*p.rate);
     
     p.msTolerance=1;
-    p.rate=11;
-    %p.n=Duration*p.rate;
-    p.n = 3000;
+    p.rate=10;
+    p.n=Duration*p.rate;
+%     p.n = 3000;
     % p.addr = getPPaddr;
     p = init(pco(p));
 end
