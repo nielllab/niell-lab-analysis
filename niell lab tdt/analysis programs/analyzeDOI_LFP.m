11166f8ac4471649d0b1c98a5640b2ad0f57846b
Cris <crisniell@gmail.com>
2017-04-12 09:43:56 -0700

    more lfp

--- niell lab tdt/analysis programs/analyzeDOI_LFP.m
+++ niell lab tdt/analysis programs/analyzeDOI_LFP.m
@@ -1,5 +1,7 @@
 %load('compileAll_withLFP_121916.mat')
 
+close all
+clear all
 
 batchDOIephys_filtered; %%% load batch file
 
@@ -14,8 +16,13 @@ clear darkSpikes
 clear stLFPall stLFPallMean stLFPallStd nsAll
 ntotal=0;
 
+prepostLabel= {'pre','post'};
 %%% calculate spike-triggered LFP
-for i = 1:length(use)
+
+neighborsite = [2 3 4 5 4 5 6 7 10 11 12 13 12 13 14 15];
+
+for i = 1:length(use)-1
+%for i = 1:3
     
     i
     afile = [pathname '\' files(use(i)).dir '\' files(use(i)).analysisfile '.mat'];
@@ -25,108 +32,119 @@ for i = 1:length(use)
     [inh mid] = getWaveform(clustfile,afile,0);
     nc = length(inh); cellrange = (ntotal+1):(ntotal+nc);
     nc
-    cellrange
-
-    if strcmp(files(use(i)).treatment,'Saline'), treatment(cellrange)=saline, end;
-    if strcmp(files(use(i)).treatment,'DOI'), treatment(cellrange)=doi, end;
-    if strcmp(files(use(i)).treatment,'5HT'), treatment(cellrange)=ht, end;
-
+    cellrange;
+    inhAll(cellrange)=inh;
+    
+    if strcmp(files(use(i)).treatment,'Saline'), treatment(cellrange)=saline; end;
+    if strcmp(files(use(i)).treatment,'DOI'), treatment(cellrange)=doi; end;
+    if strcmp(files(use(i)).treatment,'5HT'), treatment(cellrange)=ht; end;
     
     
-    %%%if ~isempty(files(use(i)).blockDark{1}) & ~isempty(files(use(i)).blockDark{2})
-    try
-    %%% get wn response
-    clear st_lfp ns st_lfpMean st_lfpStd
     
-    %%% loop over pre and post
-    for prepost = 1:2
+    if ~isempty(files(use(i)).blockDrift{1}) & ~isempty(files(use(i)).blockDrift{2})
+        %  try
+        %%% get wn response
+        clear st_lfp ns st_lfpMean st_lfpStd
         
-        %%% load in spikes, LFP, and running speed
-        spikes = getSpikes(clustfile,afile,files(use(i)).blockDrift{prepost},0);
-        lfpraw = getLFPraw(clustfile,afile,files(use(i)).blockDrift{prepost},0);
-        spd = getSpeed(clustfile,afile,files(use(i)).blockDrift{prepost},0);
-        
-        lfp = median(lfpraw.data,2);  %%% take median across all sites (low noise, but maybe lose local effects?)
-        lfpsites = lfpraw.data;       %%% data for each "tetrode"
-        dt =mean(diff(lfpraw.t))
-        
-        %%% loop over all cells
-        for j = 1:length(cellrange)
-            %   j
+        %%% loop over pre and post
+        for prepost = 1:2
             
-            %%% interpolate speed at each of the spike times
-            spikespeed = interp1(spd.t,spd.v,spikes.sp{j});
+            %%% load in spikes, LFP, and running speed
+            spikes = getSpikes(clustfile,afile,files(use(i)).blockDrift{prepost},0);
+            lfpraw = getLFPraw(clustfile,afile,files(use(i)).blockDrift{prepost},0);
+            spd = getSpeed(clustfile,afile,files(use(i)).blockDrift{prepost},0);
             
-            %%% find site for this cell
-            site = ceil(cells(j,1)/4)
-            
-            %%% calculate spike-triggered LFP for stationary and moving
-            for move =1:2
-                s = spikes.sp{j};
-                darkSpikes{cellrange(j),prepost} = s;
-                
-                %%% collect all spikes (with buffer at beginning/end for window, at appropriate speed
-                if move==1
-                    s = s(s>1.5 & s<max(lfpraw.t)-1.5 &spikespeed<0.5);
-                else
-                    s = s(s>1.5 & s<max(lfpraw.t)-1.5 &spikespeed>1);
-                end
-                
-                clear lfpsegs                
-                %%% for each spike, grab the lfp segment around it
-                for n = 1:length(s);
-                    ind = round(s(n)/dt);
-                    lfpsegs(n,:) = lfpsites(ind-768:ind+768,site);
-                end
-                
-                
-                %%% average lfp segments
-                if length(s)>0
-                    st_lfp(j,:,prepost,move) = median(lfpsegs,1);
-                    st_lfpMean(j,:,prepost,move) = mean(lfpsegs,1);
-                    st_lfpStd(j,:,prepost,move) = std(lfpsegs,[],1);
-                    ns(j,prepost,move) = length(s);
-                else
-                    st_lfp(j,:,prepost,move) = NaN;
-                    st_lfpMean(j,:,prepost,move) = NaN;
-                    st_lfpStd(j,:,prepost,move) = NaN;
-                    ns(j,prepost,move) = NaN;
-                end
+            lfp = median(lfpraw.data,2);  %%% take median across all sites (low noise, but maybe lose local effects?)
+            lfpsitesRaw = double(lfpraw.data);       %%% data for each "tetrode"
+            dt =mean(diff(lfpraw.t))
+            if ~isempty(dt)
+                Fs = 1/dt;
             end
-        end  
-    end
-    
-    %%% plot figures for each cell (pre/post, move/stationary)
-    for j = 1:length(cellrange);
-
-        thisLFP = st_lfpMean(j,:,:,:);
-        range = [min(thisLFP(:)) max(thisLFP(:))];
-        figure
-        for prepost=1:2
-            for mv = 1:2
-                subplot(2,2,2*(prepost-1)+mv)
-                plot((1:length(lfpsegs))*(lfpraw.t(2)-lfpraw.t(1)),squeeze(st_lfpMean(j,:,prepost,mv)));
-                ylim(range); xlim([0 2])
-                title(sprintf('n=%d %s mv%d prepost%d',ns(j,prepost,mv),files(use(i)).treatment,mv,prepost))
-                drawnow
+            fpass = [1 15];
+            [b,a] = butter(4,fpass*2/Fs);
+            clear lfpsites
+            for ch = 1:16
+                lfpsites(:,ch) = filtfilt(b,a,lfpsitesRaw(:,ch));
+            end
+            
+            %%% loop over all cells
+            for j = 1:length(cellrange)
+                %   j
+                
+                %%% interpolate speed at each of the spike times
+                spikespeed = interp1(spd.t,spd.v,spikes.sp{j});
+                
+                %%% find site for this cell
+                site = ceil(cells(j,1)/4);
+                
+                %%% calculate spike-triggered LFP for stationary and moving
+                for move =1:2
+                    s = spikes.sp{j};
+                    darkSpikes{cellrange(j),prepost} = s;
+                    
+                    %%% collect all spikes (with buffer at beginning/end for window, at appropriate speed
+                    if move==1
+                        s = s(s>1.5 & s<max(lfpraw.t)-1.5 &spikespeed<0.5);
+                    else
+                        s = s(s>1.5 & s<max(lfpraw.t)-1.5 &spikespeed>1);
+                    end
+                    
+                    clear lfpsegs
+                    %%% for each spike, grab the lfp segment around it
+                    for n = 1:length(s);
+                        ind = round(s(n)/dt);
+                        lfpsegs(n,:) = lfpsites(ind-768:ind+768,neighborsite(site));
+                    end
+                    
+                    
+                    %%% average lfp segments
+                    if length(s)>0
+                        st_lfp(j,:,prepost,move) = median(lfpsegs,1);
+                        st_lfpMean(j,:,prepost,move) = mean(lfpsegs,1);
+                        st_lfpStd(j,:,prepost,move) = std(lfpsegs,[],1);
+                        ns(j,prepost,move) = length(s);
+                    else
+                        st_lfp(j,:,prepost,move) = NaN;
+                        st_lfpMean(j,:,prepost,move) = NaN;
+                        st_lfpStd(j,:,prepost,move) = NaN;
+                        ns(j,prepost,move) = NaN;
+                    end
+                end
             end
         end
+        
+        %%% plot figures for each cell (pre/post, move/stationary)
+        for j = 1:length(cellrange);
+            
+            thisLFP = st_lfpMean(j,:,:,:);
+            range = [min(thisLFP(:)) max(thisLFP(:))];
+            figure
+            for prepost=1:2
+                for mv = 1:2
+                    subplot(2,2,2*(prepost-1)+mv)
+                    plot((1:size(lfpsegs,2))*(lfpraw.t(2)-lfpraw.t(1)),squeeze(st_lfpMean(j,:,prepost,mv)));
+                    ylim(range); xlim([0 2])
+                    title(sprintf('n=%d %s mv%d %s inh%d',ns(j,prepost,mv),files(use(i)).treatment,mv,prepostLabel{prepost},inh(j)))
+                    drawnow
+                end
+            end
+        end
+        
+        stLFPall(cellrange,:,:,:) = st_lfp;
+        size(stLFPall)
+        
+        stLFPallMean(cellrange,:,:,:) = st_lfpMean;
+        stLFPallStd(cellrange,:,:,:) = st_lfpStd;
+        
+        nsAll(cellrange,:,:) = ns;
     end
+    %     figure
+    %     plot((1:length(lfpsegs))*(lfpraw.t(2)-lfpraw.t(1)),squeeze(mean(st_lfp,1)));
     
-    stLFPall(cellrange,:,:,:) = st_lfp;
-    size(stLFPall)
-    
-    stLFPallMean(cellrange,:,:,:) = st_lfpMean;
-    stLFPallStd(cellrange,:,:,:) = st_lfpStd;
-    
-    nsAll(cellrange,:,:) = ns;
-%     figure
-%     plot((1:length(lfpsegs))*(lfpraw.t(2)-lfpraw.t(1)),squeeze(mean(st_lfp,1)));
-    
-    catch
-    sprintf('couldnt do %s', files(use(i)).expt)
-    
-    end
+    %     catch
+    %     sprintf('couldnt do %s', files(use(i)).expt)
+    %
+    %     end
     ntotal= ntotal+nc;
     close all
 end
@@ -134,18 +152,19 @@ figure
 imagesc(squeeze(stLFPall(:,:,1,2)),[-20 20])
 
 %%% average over treatments, and plot
- dt =mean(diff(lfpraw.t))
+dt =mean(diff(lfpraw.t))
 treatment = treatment(1:size(stLFPall,1));
 nsmin = min(min(nsAll,[],3),[],2);
 for treat = 1:2
     figure
-    meanAll = squeeze(nanmean(stLFPall(treatment==treat & nsmin'>100,:,:,:),1));
+    meanAll = squeeze(nanmean(stLFPall(treatment==treat & nsmin'>50 & inhAll(1:length(nsmin))>=0,:,:,:),1));
     
     for prepost=1:2
         for mv = 1:2
-            subplot(2,2,2*(prepost-1)+mv)
+            subplot(2,2,0*(prepost-1)+mv)
             plot((1:length(lfpsegs))*(2/length(lfpsegs)),meanAll(:,prepost,mv));
-            ylim([-40 40]); xlim([0.5 1.5])
+            hold on
+            ylim([-25 40]); xlim([0.5 1.5])
             drawnow
             title(sprintf('prepost %d mv %d',prepost,mv))
         end
@@ -158,8 +177,8 @@ end
 clear LFPallChDrift
 if ~exist('LFPallChDrift','var')
     for i = 1:length(use)
-       i
-       afile = [pathname '\' files(use(i)).dir '\' files(use(i)).analysisfile '.mat'];
+        i
+        afile = [pathname '\' files(use(i)).dir '\' files(use(i)).analysisfile '.mat'];
         clustfile = [pathname '\' files(use(i)).dir '\' files(use(i)).clusterfile '.mat'] ;
         
         clear layer
@@ -175,10 +194,10 @@ if ~exist('LFPallChDrift','var')
         if strcmp(files(use(i)).treatment,'DOI'), sessionTreatment(i)=doi; end;
         if strcmp(files(use(i)).treatment,'5HT'),sessionTreatment(i)=ht; end;
         
-    
+        
         if ~isempty(files(use(i)).blockDrift{1}) & ~isempty(files(use(i)).blockDrift{2})
-          i
-          for prepost=1:2
+            i
+            for prepost=1:2
                 lfpMoveDrift = getLfpMovement(clustfile,afile,files(use(i)).blockDrift{prepost},0);
                 
                 if size(lfpMoveDrift.meanSpect,1)==16
@@ -194,6 +213,9 @@ if ~exist('LFPallChDrift','var')
         end
     end
 end
+
+
+
 
 %%% average LFP spectra across layers, for each treatment
 
