afed895fd7d60502d93b1f250ebce312cb9523d4
Joze-Monster <niell.lab.orders@gmail.com>
2017-10-27 09:59:32 -0700

    Corrections to code
    Elliott

--- stimulus generation/makeGratingSizeOnGratingSize.m
+++ stimulus generation/makeGratingSizeOnGratingSize.m
@@ -1,4 +1,4 @@
-clear all
+clear; close all;
 
 % xsz = 128;
 xsz = 128;
@@ -63,8 +63,8 @@ for r=1:length(totalRangeCS)
     %%% g = sqrt(x.^2 + y.^2)<radiusRange(r);
     gaussianCent{r}=gCenter;
     gaussianSurr{r}=gSurr;
-    figure
-    imagesc(gaussianCent{r}+gaussianSurr{r},[0 1]);
+%     figure
+%     imagesc(gaussianCent{r}+gaussianSurr{r},[0 1]);
 end
 %%
 % for r=1:length(radiusRangeCent)
@@ -109,16 +109,16 @@ end
 thetarangeCent = linspace(0, pi,nthetaCent+1);
 thetarangeCent = thetarangeCent(1:end-1);
 thetarangeSurr = linspace(0, pi,nthetaSurr+1);
-thetarangeSurr = thetarangeSurr(1:end-1);
+thetarangeSurr = (thetarangeSurr(1:end-1));
 
 innerSurr = zeros(1,numtr);
 trial=0;
-for n= 1:length(thetarangeCent);
-    for i = 1:length(xposrange);
+for n= 1:length(thetarangeCent)
+    for i = 1:length(xposrange)
         for j = 1:length(totalRangeCS) %length(radiusRangeCent);
-            for k = 1:length(sfrange);
-                for l = 1:length(tfrange);
-                    for m= 1:length(phaserange);
+            for k = 1:length(sfrange)
+                for l = 1:length(tfrange)
+                    for m= 1:length(phaserange)
                         for o = 1:length(contrastRange)
                             for p = 1:length(thetarangeSurr)
 %                                 for q = 1:length(radiusRangeSurr)
@@ -136,11 +136,11 @@ for n= 1:length(thetarangeCent);
                                         phase(trial) = rand*pi;
                                     end
 %                                     if j < floor(length(totalRangeCS)/2)
-                                        thetaSurr(trial) = thetarangeSurr(n);
+                                        thetaSurr(trial) = thetarangeSurr(p);
 %                                     else
 %                                         thetaSurr(trial) = thetarangeSurr(2);
 %                                     end
-                                    trialID(trial) = j;
+                                    trialID(:,trial) = [j;n;p];
 %                                         
 %                                 end
                             end
@@ -177,7 +177,7 @@ end
 
 moviedata = moviedata(1:xsz,1:ysz,:);
 %%
-% % %%%preview 1/10 of movie
+%%%preview 1/10 of movie
 % figure
 % for i = 1:length(moviedata)/10
 %     i
@@ -185,7 +185,7 @@ moviedata = moviedata(1:xsz,1:ysz,:);
 % drawnow
 % end
 % 
-% %%%play a single stimulus
+%%%play a single stimulus
 % figure
 % stim = 10;
 % for i = 31+60*(stim-1):60+60*(stim-1)
@@ -205,15 +205,40 @@ moviedata = moviedata(1:xsz,1:ysz,:);
 % save sizeSelect2sf8sz26min moviedata xpos ypos tf sf phase theta framerate duration isi nx ny radius radiusRange contrasts order totalduration sizeVals
 save patchonpatch14min moviedata xpos ypos tf sf phase thetaCent framerate duration isi nx ny radiusCent radiusSurr CenterOn SurroundOn totalRangeCS contrasts order totalduration -v7.3
 %%
+for ii=1:size(totalRangeCS,2)
+%     temp1(ii,1) = sum(trialID(1,:) == 1 & trialID(2,:) == 1 & trialID(3,:)==1);
+%     temp2(ii,1) = sum(trialID(1,:) == 1 & trialID(2,:) == 1 & trialID(3,:)==2);
+%     temp3(ii,1) = sum(trialID(1,:) == 1 & trialID(2,:) == 2 & trialID(3,:)==1);
 
-for tr = 1:24:trial
-    tr
+    temp1(ii,:) = find(trialID(1,:)==ii & trialID(2,:)==1 & trialID(3,:)==1); %temp1=temp1(1);
+    temp2(ii,:) = find(trialID(1,:)==ii & trialID(2,:)==1 & trialID(3,:)==2); %temp1=temp1(1);
+    temp3(ii,:) = find(trialID(1,:)==ii & trialID(2,:)==2 & trialID(3,:)==1); %temp1=temp1(1);
+    temp4(ii,:) = find(trialID(1,:)==ii & trialID(2,:)==2 & trialID(3,:)==2); %temp1=temp1(1);
+end
+
+tempT = ([temp1(:,1) temp2(:,1) temp3(:,1) temp4(:,1)]);
+tempT = tempT(:);
+moviedata = zeros(xsz,ysz,14,'uint8')+128; i=1;
+
+for ii = 1:size(tempT)
+    
+    tr=tempT(ii);
     phCent = (x*cos(thetaCent(tr)) + y*sin(thetaCent(tr)))*2*pi*sf(tr) + phase(tr);
     phSurr = (x*cos(thetaSurr(tr)) + y*sin(thetaSurr(tr)))*2*pi*sf(tr) + phase(tr);
 %     if thetaCent(tr)~=2*pi || thetaSurr(tr) ~=2*pi
-        for t = 1:duration*framerate;
+        for t = 1:duration*framerate
             frame = uint8(0.5*255*(((cos(phCent + 2*pi*t*tf(tr)/framerate).*(gaussianCent{radiusCent(tr)})) + (cos(phSurr + 2*pi*t*tf(tr)/framerate).*(gaussianSurr{radiusSurr(tr)}))   )*contrasts(tr)+1));
+            moviedata(xpos(tr):xpos(tr)+blockwidth-1, ypos(tr):ypos(tr)+blockwidth-1,i) = frame; 
+        end
+        if binarize
+            moviedata(moviedata>128)=255;
+            moviedata(moviedata<128)=0;
         end
 %     end
-    figure; imagesc(frame);
+    figure; 
+    %imshow(moviedata(:,:,i));
+    imagesc(frame);
+%     title1 = ['stim' num2str(i) '.jpg'];
+%     imwrite(moviedata(:,:,i),title1);
+    i=i+1;
 end
