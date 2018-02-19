9b004d191c0912452363d722998296a7e3472a9f
Cris <crisniell@gmail.com>
2018-02-19 15:56:35 -0800

    lgn

--- niell lab tdt/analysis programs/compile data/makeLGNdevFigs.m
+++ niell lab tdt/analysis programs/compile data/makeLGNdevFigs.m
@@ -44,37 +44,37 @@ mean_spectrum = mean_spectrum / max(max(mean_spectrum));
 
 
 
-
-%%% example rfs
-for a = [1 4]
-    a
-    figure
-   set(gcf,'Name',sprintf('age %d',ageBins(1,a)));
-   use = find(genotype==2 & age>=ageBins(1,a) & age<=ageBins(2,a));
-   length(use)
-    if length(use)>100
-        use = use(1:100);
-    end
-    for n= 1:length(use);
-        subplot(10,10,n);
-        if ~isempty(wn_all(use(n)).svd_xy)
-            %%%  imagesc(wn_all(use(n)).sta_final,[-0.1 0.1]); axis equal; colormap jet
-            sta = squeeze(wn_all(use(n)).svd_xy(1,:,:));
-            sta_fix = ifft2(fft2(sta).*mean_spectrum);
-            
-            imagesc(sta_fix,[-0.025 0.025]); axis equal; colormap gray
-        else          
-            imagesc(0,[-0.1 0.1]), colormap gray;
-        end
-        axis off
-        set(gca,'LooseInset',get(gca,'TightInset'))
-        title(sprintf('%d',use(n)))
-    end
-    drawnow
-end
-
-
-
+% 
+% %%% example rfs
+% for a = [1 4]
+%     a
+%     figure
+%    set(gcf,'Name',sprintf('age %d',ageBins(1,a)));
+%    use = find(genotype==2 & age>=ageBins(1,a) & age<=ageBins(2,a));
+%    length(use)
+%     if length(use)>100
+%         use = use(1:100);
+%     end
+%     for n= 1:length(use);
+%         subplot(10,10,n);
+%         if ~isempty(wn_all(use(n)).svd_xy)
+%             %%%  imagesc(wn_all(use(n)).sta_final,[-0.1 0.1]); axis equal; colormap jet
+%             sta = squeeze(wn_all(use(n)).svd_xy(1,:,:));
+%             sta_fix = ifft2(fft2(sta).*mean_spectrum);
+%             
+%             imagesc(sta_fix,[-0.025 0.025]); axis equal; colormap gray
+%         else          
+%             imagesc(0,[-0.1 0.1]), colormap gray;
+%         end
+%         axis off
+%         set(gca,'LooseInset',get(gca,'TightInset'))
+%         title(sprintf('%d',use(n)))
+%     end
+%     drawnow
+% end
+% 
+% 
+% 
 
 %%% extract flash values
 for i = 1:length(fl_all);
@@ -163,37 +163,37 @@ for i= 1:length(all)
     set(gca,'LooseInset',get(gca,'TightInset'))
 end
 
-%%% show all on/off vs age
-for a = [2]
-    a
-    figure
-       set(gcf,'Name',sprintf('age %d',ageBins(1,a)));
-    use = find(genotype==2 & age>=ageBins(1,a) & age<=ageBins(2,a));
-    if length(use)>36
-        use = use(1:36);
-    end
-    for n= 1:length(use);
-        subplot(6,6,n);
-        
-        rf = squeeze(fl_all(use(n)).rf(:,:,2) - fl_all(use(n)).rf(:,:,1))';
-        m = max(max(abs(rf)));
-        onoffIm = mat2im(fl_all(use(n)).onoffbias',cmap,[-1.5 1.5]);
-        amp = fl_all(use(n)).zabs/15;  amp(amp>1)=1; amp = repmat(amp,[1 1 3]);
-        imshow(onoffIm.*amp);  title(sprintf('%0.2f',fl_all(use(n)).onoffoverlap));
-        sustIm = mat2im(fl_all(use(n)).sustainBias',cmap,[0.25 0.75]);
-        imshow(sustIm.*amp); title(sprintf('%0.2f',fl_all(use(n)).sustVariation));
-        % imagesc(rf,[-10 10]);
-        % imagesc(squeeze(wn_all(use(n),1).svd_xy(1,:,:)),[-0.1 0.1]);
-        % imagesc(abs(fl_all(use(n)).zscore(:,:,1)') + abs(fl_all(use(n)).zscore(:,:,2)')   ,[2 20]);
-        % imagesc(fl_all(use(n)).zabs,[2 20])
-        
-        %imagesc(fl_all(use(n)).onoffbias',[-1 1]);
-        axis equal; colormap jet;  axis off
-        set(gca,'LooseInset',get(gca,'TightInset'))
-        title(sprintf('%d',use(n)))
-    end
-    drawnow
-end
+% %%% show all on/off vs age
+% for a = 1:4
+%     a
+%     figure
+%        set(gcf,'Name',sprintf('age %d sust',ageBins(1,a)));
+%     use = find(genotype==2 & age>=ageBins(1,a) & age<=ageBins(2,a));
+%     if length(use)>100
+%         use = use(1:100);
+%     end
+%     for n= 1:length(use);
+%         subplot(10,10,n);
+%         
+%         rf = squeeze(fl_all(use(n)).rf(:,:,2) - fl_all(use(n)).rf(:,:,1))';
+%         m = max(max(abs(rf)));
+%         onoffIm = mat2im(fl_all(use(n)).onoffbias',cmap,[-1.5 1.5]);
+%         amp = fl_all(use(n)).zabs/15;  amp(amp>1)=1; amp = repmat(amp,[1 1 3]);
+%         imshow(onoffIm.*amp);  title(sprintf('%0.2f %d',fl_all(use(n)).onoffoverlap,use(n)));
+%         sustIm = mat2im(fl_all(use(n)).sustainBias',cmap,[0 0.75]);
+%         imshow(sustIm.*amp); title(sprintf('%0.2f %d',fl_all(use(n)).sustVariation,use(n)));
+%         % imagesc(rf,[-10 10]);
+%         % imagesc(squeeze(wn_all(use(n),1).svd_xy(1,:,:)),[-0.1 0.1]);
+%         % imagesc(abs(fl_all(use(n)).zscore(:,:,1)') + abs(fl_all(use(n)).zscore(:,:,2)')   ,[2 20]);
+%         % imagesc(fl_all(use(n)).zabs,[2 20])
+%         
+%         %imagesc(fl_all(use(n)).onoffbias',[-1 1]);
+%         axis equal; colormap jet;  axis off
+%         set(gca,'LooseInset',get(gca,'TightInset'))
+%   
+%     end
+%     drawnow
+% end
 
 plotDevGenoData(abs(onoffoverlap'),age,agelist, ageBins,~isnan(onoffoverlap), genotype,'onoff overlap')
 scatterDevGenoData(abs(onoffoverlap'),age,agelist, ageBins,~isnan(onoffoverlap), genotype,'onoff overlap')
@@ -278,11 +278,11 @@ legend('grating','spots')
 
 %%% grrating responsive fraction
 
-plotDevwtData(sf_amp'>1,age,agelist, ageBins,1, genotype,'% responsive gratings (>2sp/sec)')
+plotDevwtData(fl_amp'>2,age,agelist, ageBins,1, genotype,'fraction responsive',0)
 ylim([0 1])
-plotDevwtData(fl_amp'>1,age,agelist, ageBins,1, genotype,'% responsive gratings (>2sp/sec)',1)
+plotDevwtData(sf_amp'>2,age,agelist, ageBins,1, genotype,'fraction responsive',1)
 ylim([0 1])
-legend('gratings','spots')
+legend('spots','gratings')
 
 
 
@@ -371,12 +371,13 @@ ylim([0 0.5])
 %%% figure 1
 figure
 all = [256 361 184];
-all = [256 361 39 14 184 187]
+all = [256 361 39  186 184  530]
 for i= 1:length(all)
     subplot(1,6,i);
            sta = squeeze(wn_all(all(i)).svd_xy(1,:,:));
             sta_fix = ifft2(fft2(sta).*mean_spectrum);
-            if i==5 | i==6, range = [-0.05 0.05], else range = [-0.025 0.025], end
+            if i==5 | i==4, range = [-0.05 0.05], else range = [-0.025 0.025], end
+            if i==6, range = [-0.04 0.04]; end
             imagesc(sta_fix,range); axis equal; colormap gray
             %if i==3, title('adult'), else title('P16'), end
             hold on
@@ -385,6 +386,7 @@ for i= 1:length(all)
            end
     axis off
     axis([17 17+75 1 76])
+    if i==6, axis([1 76 1 76]); end
     set(gca,'LooseInset',get(gca,'TightInset'))
 end
 
@@ -432,8 +434,7 @@ multcompare(stats,'Ctype','dunn-sidak')
 [p tbl stats] = kruskalwallis(data,group)
 multcompare(stats,'Ctype','dunn-sidak')
 
-
-%%% Figure 2
+%%% figure 2
 figure
 subplot(2,3,1);
 use = age>=ageBins(1,1) & age<=ageBins(2,1) & genotype==2;
@@ -447,6 +448,7 @@ box off
 % figure
 % kruskalwallis(fl_sztune(use & fl_amp>fl_thresh,:))
 
+  
 subplot(2,3,2);
 use = age>=ageBins(1,end) & age<=ageBins(2,end) & genotype==2;
 errorbar(mean(fl_sztune(use & fl_amp>fl_thresh,:)),std(fl_sztune(use & fl_amp>fl_thresh,:))/sqrt(sum(use & fl_amp>fl_thresh)),'k');
@@ -458,6 +460,23 @@ set(gca,'Xticklabel',{'1','2','4','8','16','full'});
 box off
 % figure
 % kruskalwallis(fl_sztune(use & fl_amp>fl_thresh,:))
+
+% figure
+% subplot(2,3,2);
+% use = age>=ageBins(1,1) & age<=ageBins(2,1) & genotype==2;
+% errorbar((1:6)-0.1,mean(fl_sztune(use & fl_amp>fl_thresh,:)),std(fl_sztune(use & fl_amp>fl_thresh,:))/sqrt(sum(use & fl_amp>fl_thresh)),'k');
+% hold on
+% use = age>=ageBins(1,end) & age<=ageBins(2,end) & genotype==2;
+% errorbar((1:6)+0.1,mean(fl_sztune(use & fl_amp>fl_thresh,:)),std(fl_sztune(use & fl_amp>fl_thresh,:))/sqrt(sum(use & fl_amp>fl_thresh)),'k');
+% xlim([0.5 6.5]); ylim([0 5.5])
+% ylabel('sp/sec'); xlabel('spot radius (deg)')
+% legend('P14-16','adult')
+% set(gca,'Xtick',1:6);
+% set(gca,'Xticklabel',{'1','2','4','8','16','full'});
+% box off
+% figure
+% kruskalwallis(fl_sztune(use & fl_amp>fl_thresh,:))
+
 
 % subplot(2,3,3)
 % use = age>=ageBins(1,2) & age<=ageBins(2,2) & genotype==2;
@@ -487,6 +506,7 @@ ylim([0 0.75])
 subplot(2,3,5)
 [resp err] = plotDevwtData(sfs(sf_inds)',age,agelist, ageBins,sf_amp>2 & genotype==2, genotype,'peak sf (cpd)',1)
 
+
 % %%%DS OS mean
 subplot(2,3,6)
 [resp err] = plotDevwtData(driftOSI(:,1),age-0.1,agelist-0.1, ageBins-0.1,sf_amp>2, genotype,'mean OSI',1)
@@ -494,13 +514,23 @@ subplot(2,3,6)
 ylim([0 0.5])
 legend('OSI','DSI')
 
-% %%% DS/OS fraction
+figure
+subplot(2,3,6)
+plotDevwtData(fl_amp'>2,age,agelist, ageBins,1, genotype,'fraction responsive',1)
+ylim([0 1])
+plotDevwtData(sf_amp'>2,age,agelist, ageBins,1, genotype,'fraction responsive',1)
+ylim([0 1])
+legend('spots','gratings')
+axis square
+
+%%% DS/OS fraction
+% figure
 % subplot(2,3,6)
 % plotDevwtData(driftOSI(:,1)>0.25,age,agelist, ageBins,sf_amp>2, genotype,'fraction',1)
 % plotDevwtData(driftDSI(:,1)>0.25,age,agelist, ageBins,sf_amp>2, genotype,'fraction',1)
 % ylim([0 1])
 % legend('OS','DS')
-
+% axis square
 
 
 
@@ -526,23 +556,32 @@ multcompare(stats,'Ctype','dunn-sidak')
 [p tbl stats] = kruskalwallis(data(group>0),group(group>0))
 multcompare(stats,'Ctype','dunn-sidak')
 
+[data group] = labelbyage(fl_amp>2,age,ageBins,genotype==2 );
+[p tbl stats] = kruskalwallis(data(group>0),group(group>0))
+multcompare(stats,'Ctype','dunn-sidak')
 
+[data group] = labelbyage(sf_amp>2,age,ageBins,genotype==2 );
+[p tbl stats] = kruskalwallis(data(group>0),group(group>0))
+multcompare(stats,'Ctype','dunn-sidak')
 
 
 %%% figure 3
 figure
-
-
 all = [212 159 185]
-
-for i= 1:length(all)
+all = [ 212 101  338 185 199]
+for i= 1:4
     subplot(2,4,i);
     onoffIm = mat2im(fl_all(all(i)).onoffbias',cmap,[-1.35 1.35]);
-      amp = fl_all(all(i)).zabs/8;  amp = amp-0.3; amp(amp>1)=1; amp(amp<0)=0; amp = repmat(amp,[1 1 3]); 
+      amp = fl_all(all(i)).zabs/8;  if i ==5 amp = amp-0.5; else amp = amp-0.3;end;
+      amp(amp>1)=1; amp(amp<0)=0; amp = repmat(amp,[1 1 3]); 
 
     imshow(imresize(onoffIm.*amp,2));  %title(sprintf('%0.2f',fl_all(all(i)).onoffoverlap));
     if i ==1
         axis([21 143 1 123]); title('P16')
+           elseif i ==5
+          axis([123 244 1 123]);
+    elseif i ==2
+        axis([61 183 1 123])
     else       
         axis([ 81 203 1 123]); title('adult')
     end
@@ -550,18 +589,22 @@ for i= 1:length(all)
     axis off
     set(gca,'LooseInset',get(gca,'TightInset'))
 end
-subplot(2,4,4)
-[resp err]=plotDevGenoData(abs(onoffoverlap'),age,agelist, ageBins,~isnan(onoffoverlap) & genotype==2, genotype,'onoff segregation',1)
-ylim([ 0 1])
+
 
 all = [ 212 159 185]
-for i= 1:length(all)
+all = [ 212 101 338 185  199]
+for i= 1:4
     subplot(2,4,4+i);
-    amp = fl_all(all(i)).zabs/8;  amp = amp-0.3; amp(amp>1)=1; amp(amp<0)=0; amp = repmat(amp,[1 1 3]); 
-    sustIm = mat2im(fl_all(all(i)).sustainBias',cmap,[0 0.75]);
+    amp = fl_all(all(i)).zabs/8;  if i ==5 amp = amp-0.5; else amp = amp-0.3;end;
+    amp(amp>1)=1; amp(amp<0)=0; amp = repmat(amp,[1 1 3]); 
+    sustIm = mat2im(fl_all(all(i)).sustainBias',cmap,[0 .75]);
     imshow(imresize(sustIm.*amp,2)); % title(sprintf('%0.2f',fl_all(all(i)).sustVariation));
       if i ==1
         axis([21 143 1 123]); title('P16')
+      elseif i ==5
+          axis([123 244 1 123]);
+              elseif i ==2
+        axis([61 183 1 123])
     else       
         axis([ 81 203 1 123]); title('adult')
       end
@@ -570,8 +613,16 @@ for i= 1:length(all)
     set(gca,'LooseInset',get(gca,'TightInset'))
 end
 
+figure
+subplot(1,3,1)
+[resp err]=plotDevGenoData(~isnan(onoffoverlap'),age,agelist, ageBins, genotype==2, genotype,'fraction mapped',1)
+ylim([ 0 1])
 
-subplot(2,4,8)
+subplot(1,3,2)
+[resp err]=plotDevGenoData(abs(onoffoverlap'),age,agelist, ageBins,~isnan(onoffoverlap) & genotype==2, genotype,'on/off uniformity',1)
+ylim([ 0 1])
+
+subplot(1,3,3)
 [resp err]= plotDevGenoData(1-abs(sustVariation'),age,agelist, ageBins,~isnan(sustVariation)& genotype==2, genotype,'sustain uniformity',1)
 ylim([ 0 1])
 
