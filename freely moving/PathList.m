e6de96e45817486ddf2225a7b6276d1bfa34b5b6
angiemikha <31757559+angiemikha@users.noreply.github.com>
2019-08-15 17:35:18 -0700

    pathlist
    experiments used for analysis
    
    angie

---
+++ freely moving/PathList.m
@@ -0,0 +1,78 @@
+%animal path list list
+
+%% J462a(green)% 
+pname={'T:\PreyCapture\Cohort3\J462a(green)\080919\Approach\';
+    'T:\PreyCapture\Cohort3\J462a(green)\080719\Approach\';
+    'T:\PreyCapture\Cohort3\J462a(green)\080219\Approach\';
+    'T:\PreyCapture\Cohort3\J462a(green)\073019\Approach\'};
+%     'T:\PreyCapture\Cohort3\J462a(green)\071019\Approach\';
+%     'T:\PreyCapture\Cohort3\J462a(green)\070419\Approach\'};
+%% J462b(blue) 
+pname={'T:\PreyCapture\Cohort3\J462b(blue)\080919\Approach\';
+    'T:\PreyCapture\Cohort3\J462b(blue)\080719\Approach\';
+    'T:\PreyCapture\Cohort3\J462b(blue)\080219\Approach\';
+    'T:\PreyCapture\Cohort3\J462b(blue)\073019\Approach\'};
+    %'T:\PreyCapture\Cohort3\J462b(blue)\070219\Approach\'};
+
+%% J462c(pink)
+pname={'T:\PreyCapture\Cohort3\J462c(pink)\080919\Approach\';
+    'T:\PreyCapture\Cohort3\J462c(pink)\080719\Approach\';
+    'T:\PreyCapture\Cohort3\J462c(pink)\080219\Approach\';
+    'T:\PreyCapture\Cohort3\J462c(pink)\073019\Approach\'};
+%     'T:\PreyCapture\Cohort3\J462c(pink)\071019\Approach\';
+%     'T:\PreyCapture\Cohort3\J462c(pink)\070419\Approach\'};
+%% J463b(white) %
+pname ={'T:\PreyCapture\Cohort3\J463b(white)\081219\Approach\';
+    'T:\PreyCapture\Cohort3\J463b(white)\080819\Approach\';
+    'T:\PreyCapture\Cohort3\J463b(white)\080519\Approach\';
+    'T:\PreyCapture\Cohort3\J463b(white)\080119\Approach\';
+    'T:\PreyCapture\Cohort3\J463b(white)\072819\Approach\'};
+%     'T:\PreyCapture\Cohort3\J463b(white)\071119\Approach\';
+%     'T:\PreyCapture\Cohort3\J463b(white)\070819\Approach\';
+%     'T:\PreyCapture\Cohort3\J463b(white)\070519\Approach\';
+%     'T:\PreyCapture\Cohort3\J463b(white)\070319\Approach\'};
+%% J463c(blue) % done
+pname={'T:\PreyCapture\Cohort3\J463c(blue)\081219\Approach\';
+    'T:\PreyCapture\Cohort3\J463c(blue)\080819\Approach\';
+   'T:\PreyCapture\Cohort3\J463c(blue)\080519\Approach\';
+    'T:\PreyCapture\Cohort3\J463c(blue)\080119\Approach\';
+    'T:\PreyCapture\Cohort3\J463c(blue)\072819\Approach\'};
+%     'T:\PreyCapture\Cohort3\J463c(blue)\071119\Approach\';
+%     'T:\PreyCapture\Cohort3\J463c(blue)\070819\Approach\';
+%     'T:\PreyCapture\Cohort3\J463c(blue)\070519\Approach\';
+%     'T:\PreyCapture\Cohort3\J463c(blue)\070319\Approach\'};
+
+%% J463d(green) % done
+pname={'T:\PreyCapture\Cohort3\J463d(green)\081419\Approach\'
+    'T:\PreyCapture\Cohort3\J463d(green)\080819\Approach\'
+    'T:\PreyCapture\Cohort3\J463d(green)\080519\Approach\'};  
+% 'T:\PreyCapture\Cohort3\J463d(green)\071119\Approach\';
+%     'T:\PreyCapture\Cohort3\J463d(green)\070819\Approach\';
+%     'T:\PreyCapture\Cohort3\J463d(green)\070519\Approach\'};
+
+%% J465d(black) % done
+pname={'T:\PreyCapture\Cohort4\J465d(black)\081219\Approach\';
+    'T:\PreyCapture\Cohort4\J465d(black)\080719\Approach\';
+    'T:\PreyCapture\Cohort4\J465d(black)\080119\Approach\';
+    'T:\PreyCapture\Cohort4\J465d(black)\072919\Approach\'};
+%     'T:\PreyCapture\Cohort4\J465d(black)\071119\Approach\';
+%     'T:\PreyCapture\Cohort4\J465d(black)\070919\Approach\'};
+%% J475c(green) % done
+pname={'T:\PreyCapture\Cohort4\J475c(green)\081219\Approach\'
+    'T:\PreyCapture\Cohort4\J475c(green)\080719\Approach\';
+    'T:\PreyCapture\Cohort4\J475c(green)\072919\Approach\'};
+%     'T:\PreyCapture\Cohort4\J475c(green)\071119\Approach\';
+%     ' T:\PreyCapture\Cohort4\J475c(green)\070919\Approach\';
+%     'T:\PreyCapture\Cohort4\J475c(green)\070419\Approach\'};
+%% J470a(blue) %
+pname={'T:\PreyCapture\Cohort4\J470a (blue)\080919\Approach\'
+    'T:\PreyCapture\Cohort4\J470a (blue)\080519\Approach\';
+    'T:\PreyCapture\Cohort4\J470a (blue)\073019\Approach\'};
+    %'T:\PreyCapture\Cohort4\J470a (blue)\071019\Approach\'};
+
+%% J470b(pink) %
+pname={'T:\PreyCapture\Cohort4\J470b (pink)\080919\Approach\';
+    'T:\PreyCapture\Cohort4\J470b (pink)\080519\Approach\';
+    'T:\PreyCapture\Cohort4\J470b (pink)\073019\Approach\'};
+%     'T:\PreyCapture\Cohort4\J470b (pink)\070519\Approach\'};
+
