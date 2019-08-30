05b5a14780766c8d06f75ef43d9d05e0616b01c5
crisniell <crisniell@gmail.com>
2019-08-30 12:36:19 -0700

    Update nanxcorr.m

--- freely moving/nanxcorr.m
+++ freely moving/nanxcorr.m
@@ -5,18 +5,34 @@ function [cc lags] = nanxcorr(x,y,maxlag,normalization);
 %%% doing auto-correlation, which doesn't depend on relative timing
 %%%
 %%% uses same inputs/outputs as standard xcorr
+%%% except normalization option = 'zero' does mean subtraction before coeff normalization
 %%%
 %%% cmn 08/2019
 
-
+%%% check inputs
 if ~exist('normalization','var')
     normalization = 'coeff';
 end
-
 if ~exist('maxlag','var')
     maxlag = 25;
 end
 
+if normalization == 'zero'
+    x = x-nanmean(x);
+    y = y-nanmean(y);
+    normalization = 'coeff';
+end
+
+
+%%% inputs must be vectors, of same orientation (so we make them rows)
+if ~isvector(x) | ~isvector(y)
+    display('must be vectors');
+    return
+end
+if ~isrow(x), x = x'; end
+if ~isrow(y), y = y'; end
+
+%%% calculate correlation across lags
 lags = -maxlag:maxlag;
 for i = 1:length(lags);
     yshift = circshift(y,lags(i));
