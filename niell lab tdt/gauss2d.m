0eb957203d1ee0bb2764f23bab07e995da3012bd
Cris <cniell@uoregon.edu>
2019-09-16 20:21:19 -0700

    fitting code, simplified and commented for Guido lab

--- niell lab tdt/gauss2d.m
+++ niell lab tdt/gauss2d.m
@@ -1,4 +1,11 @@
 function z = gauss2d(p,x);
+%%% calculate 2d gaussian based on parameters
+%%% p = [A B x0 y0 sigx sigy], where ...
+%%% A = amplitude of baseline
+%%% B = baseline
+%%% x0,y0 = center coordinates
+%%% sigx, sigy = width of gaussian
+
 a= p(1);
 b=p(2);
 x0=p(3);
